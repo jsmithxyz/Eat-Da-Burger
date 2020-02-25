8239762f599a8f9e8568aedb86ed4c0226ced5cf
jsmithxyz <jordansmith3813@gmail.com>
2020-02-25 12:27:55 -0500

    created and populated db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+-- Seed Values 
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (burger_name) VALUES ('Portobello Mushroom Burger');
