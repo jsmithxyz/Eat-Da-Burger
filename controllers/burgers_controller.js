c7468cfaa229c863f8da3cd570b6f05a159ea7e1
jsmithxyz <jordansmith3813@gmail.com>
2020-02-28 15:32:34 -0500

    completed basic functionality of app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const router = express.Router();
+var express = require("express");
 
-// Import the model (cat.js) to use its database functions.
-const burger = require("../models/burger.js");
+var router = express.Router();
+
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", (req, res) => {
+router.get("/", function(req, res) {
   burger.all(function(data) {
-    let hbsObject = {
+    var hbsObject = {
       burgers: data
     };
     console.log(hbsObject);
@@ -15,46 +16,25 @@ router.get("/", (req, res) => {
   });
 });
 
-router.post("/api/burgers", (req, res) => {
-  burger.create([
-    "burger_name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
+router.post("/api/burgers", function(req, res) {
+  burger.create(["burger_name", "devoured"], [req.body.name, req.body.devoured], function(result) {
+
     res.json({ id: result.insertId });
   });
 });
 
-router.put("/api/burgers/:id", (req, res) => {
-  let burger_status = "id = " + req.params.id;
+router.put("/api/burgers/:id", function(req, res) {
+    const condition = `id = ${req.params.id}`;
 
-  console.log("burger_status", burger_status);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, burger_status, (result) => {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, result => {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
 });
 
-router.delete("/api/burgers/:id", function(req, res) {
-  var burger_status = "id = " + req.params.id;
-
-  burgers.delete(burger_status, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-// Export routes for server.js to use.
 module.exports = router;
---
+++ public/assets/js/burger.js
@@ -0,0 +1,47 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-dev").on("click", function(event) {
+        console.log("HITTTTT!!!");
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdev");
+  
+      var newState = {
+        devoured: newDevoured
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newState
+      }).then(
+        function() {
+          console.log(`changed devoured state to ${newState}`);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#burg").val().trim(),
+        devoured: $("[name=devoured]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
